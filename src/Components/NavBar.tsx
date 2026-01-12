61592b09a572bb10d3653cc65400080c7a2a12c4
ibrahimAnas <anas.jameel.dev@pk.see.biz>
2026-01-12 18:12:45 +0500

    ch

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -4,7 +4,6 @@ const NavBar = () => {
   return (
      <nav className="header__nav">
         <a href="#features">Features</a>
-        <a href="#pricing">Pricing</a>
         <a href="#contact">Contact</a>
       </nav>
   )
